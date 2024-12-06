d8c426974d1d389d7fce388f0c1879eeb70ad85c
Giole <gudwns4f12@gmail.com>
2024-12-06 21:05:03 +0900

    Update

--- @UnityScripts/Editor/UnitySceneChanger.cs
+++ @UnityScripts/Editor/UnitySceneChanger.cs
@@ -7,24 +7,26 @@ using UnityEngine.SceneManagement;
 
 // 참고 : https://kkk159147.tistory.com/6
 // 씬 이동을 단축키로 좀더 편하게 이동할 수 있도록 도와주는 에디터 스크립트
-public class SceneChangeEditor : Editor
+public class UnitySceneChangerEditor : Editor
 {
+    const string SCENE_PATH = "Assets/@Scenes/";
+
     // 현재 씬 저장, 해당 경로 씬 파일 오픈
-    private static void LoadScene(string path)
+    private static void LoadScene(string sceneName)
     {
         Scene currentScene = SceneManager.GetActiveScene();
         EditorSceneManager.SaveScene(currentScene);
-        EditorSceneManager.OpenScene(path);
+        EditorSceneManager.OpenScene(SCENE_PATH + sceneName);
     }
 
     [MenuItem("Tools/SceneChange/TitleScene &1")]
-    private static void TitleScene() => LoadScene("Assets/@Scenes/TitleScene.unity");
+    private static void TitleScene() => LoadScene("TitleScene.unity");
 
     [MenuItem("Tools/SceneChange/GameScene &2")]
-    private static void GameScene() => LoadScene("Assets/@Scenes/GameScene.unity");
+    private static void GameScene() => LoadScene("GameScene.unity");
 
     [MenuItem("Tools/SceneChange/DevScene &`")]
-    private static void DevScene() => LoadScene("Assets/@Scenes/DevScene.unity");
+    private static void DevScene() => LoadScene("DevScene.unity");
 
 
 
