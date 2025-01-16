e300cdafc71ea176033a450795b06da0024f3bb3
Giole <gudwns4f12@gmail.com>
2025-01-16 09:32:34 +0900

    Update

--- @UnityScripts/Editor/UnitySceneChanger.cs
+++ @UnityScripts/Editor/UnitySceneChanger.cs
@@ -9,24 +9,24 @@ using UnityEngine.SceneManagement;
 // 씬 이동을 단축키로 좀더 편하게 이동할 수 있도록 도와주는 에디터 스크립트
 public class UnitySceneChangerEditor : Editor
 {
-    const string SCENE_PATH = "Assets/@Scenes/";
+    //const string SCENE_PATH = "Assets/@Scenes/";
 
-    // 현재 씬 저장, 해당 경로 씬 파일 오픈
-    private static void LoadScene(string sceneName)
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-        EditorSceneManager.SaveScene(currentScene);
-        EditorSceneManager.OpenScene(SCENE_PATH + sceneName);
-    }
+    //// 현재 씬 저장, 해당 경로 씬 파일 오픈
+    //private static void LoadScene(string sceneName)
+    //{
+    //    Scene currentScene = SceneManager.GetActiveScene();
+    //    EditorSceneManager.SaveScene(currentScene);
+    //    EditorSceneManager.OpenScene(SCENE_PATH + sceneName);
+    //}
 
-    [MenuItem("Tools/SceneChange/TitleScene &1")]
-    private static void TitleScene() => LoadScene("TitleScene.unity");
+    //[MenuItem("Tools/SceneChange/TitleScene &1")]
+    //private static void TitleScene() => LoadScene("TitleScene.unity");
 
-    [MenuItem("Tools/SceneChange/GameScene &2")]
-    private static void GameScene() => LoadScene("GameScene.unity");
+    //[MenuItem("Tools/SceneChange/GameScene &2")]
+    //private static void GameScene() => LoadScene("GameScene.unity");
 
-    [MenuItem("Tools/SceneChange/DevScene &`")]
-    private static void DevScene() => LoadScene("DevScene.unity");
+    //[MenuItem("Tools/SceneChange/DevScene &`")]
+    //private static void DevScene() => LoadScene("DevScene.unity");
 
 
 
